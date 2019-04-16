d04e1f9564eeff8f4be3b56f1a40ca59323d8153
Simon <01secn@gmail.com>
2019-04-16 12:19:53 +0100

    Redirects to shelters route from index page

--- cypress/integration/viewShelters.spec.js
+++ cypress/integration/viewShelters.spec.js
@@ -1,13 +1,20 @@
 describe('find Shelters', () => {
-	it('shows a list of shelters', () => {
+  beforeEach(() => {
     cy.server()
     cy.route('GET', '**/shelters', [
       { name: 'Mile End shelter'},
       { name: 'Aldgate shelter' },
       { name: 'Some shelter' }
     ])
+  })
 
-		cy.visit('/')
+	it('shows a list of shelters', () => {
+		cy.visit('/shelters')
 		cy.get('.shelter').should('have.length', 3)
+  })
+
+  it('redirects to shelters route from index', () => {
+    cy.visit('/')
+    cy.get('.shelter').should('have.length', 3)
   })
 })
--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import ShelterList from './ShelterList';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ShelterList />
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={ShelterList}/>
+            <Route path='/shelters' component={ShelterList}/>
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
