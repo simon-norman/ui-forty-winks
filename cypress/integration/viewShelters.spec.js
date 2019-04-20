6048bb14eba5a82906cd979ee1ea209e1cb61113
matt <mwhitaker1006@gmail.com>
2019-04-20 12:24:02 +0100

    updated price in test, triggering build

--- cypress/integration/viewShelters.spec.js
+++ cypress/integration/viewShelters.spec.js
@@ -4,7 +4,7 @@ describe('find Shelters', () => {
     cy.route('GET', '**/shelters', {shelters:[
       { name: 'Mile End shelter', price: 20, description: 'here is a description', location: 'nearby' ,mapurl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2482.775190063914!2d-0.07546948426252068!3d51.51734027963678!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761caf26599a83%3A0x9b451d586c649129!2sMakers!5e0!3m2!1sen!2suk!4v1555754401793!5m2!1sen!2suk" },
       { name: 'Aldgate shelter', price: 30, description: 'here is a description also', location: 'further away', mapurl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2482.775190063914!2d-0.07546948426252068!3d51.51734027963678!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761caf26599a83%3A0x9b451d586c649129!2sMakers!5e0!3m2!1sen!2suk!4v1555754401793!5m2!1sen!2suk"  },
-      { name: 'Some shelter', price: 25, description: 'here is a description again', location: 'farthest away', mapurl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2482.775190063914!2d-0.07546948426252068!3d51.51734027963678!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761caf26599a83%3A0x9b451d586c649129!2sMakers!5e0!3m2!1sen!2suk!4v1555754401793!5m2!1sen!2suk"  }
+      { name: 'Some shelter', price: 20, description: 'here is a description again', location: 'farthest away', mapurl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2482.775190063914!2d-0.07546948426252068!3d51.51734027963678!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761caf26599a83%3A0x9b451d586c649129!2sMakers!5e0!3m2!1sen!2suk!4v1555754401793!5m2!1sen!2suk"  }
     ]})
   })
 
@@ -45,7 +45,7 @@ describe('find Shelters', () => {
       .get('.shelterName')
       .contains('Some shelter')
       .get('.shelterPrice')
-      .contains('25')
+      .contains('20')
       .get('.shelterDescription')
       .contains('here is a description again')
       .get('.shelterLocation')
