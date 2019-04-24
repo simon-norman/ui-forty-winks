a81d2bd74902310f0c088e4b9f1ece6ad674ffd9
Simon <01secn@gmail.com>
2019-04-24 17:27:39 +0100

    Tidied up view shelters test

--- cypress/integration/viewShelters.spec.js
+++ cypress/integration/viewShelters.spec.js
@@ -1,20 +1,31 @@
 describe('find Shelters', () => {
   beforeEach(() => {
     cy.route('GET', '**/shelters', {shelters:[
-      { name: 'Mile End shelter', price: 20, description: 'here is a description', location: 'nearby' ,mapurl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2482.775190063914!2d-0.07546948426252068!3d51.51734027963678!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761caf26599a83%3A0x9b451d586c649129!2sMakers!5e0!3m2!1sen!2suk!4v1555754401793!5m2!1sen!2suk" },
-      { name: 'Aldgate shelter', price: 30, description: 'here is a description also', location: 'further away', mapurl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2482.775190063914!2d-0.07546948426252068!3d51.51734027963678!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761caf26599a83%3A0x9b451d586c649129!2sMakers!5e0!3m2!1sen!2suk!4v1555754401793!5m2!1sen!2suk"  },
-      { name: 'Some shelter', price: 20, description: 'here is a description again', location: 'farthest away', mapurl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2482.775190063914!2d-0.07546948426252068!3d51.51734027963678!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761caf26599a83%3A0x9b451d586c649129!2sMakers!5e0!3m2!1sen!2suk!4v1555754401793!5m2!1sen!2suk"  }
+      { 
+        name: 'Mile End shelter', 
+        price: 20, 
+        description: 'here is a description', 
+        location: 'nearby',
+        mapurl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2482.775190063914!2d-0.07546948426252068!3d51.51734027963678!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761caf26599a83%3A0x9b451d586c649129!2sMakers!5e0!3m2!1sen!2suk!4v1555754401793!5m2!1sen!2suk" 
+      },
+      { 
+        name: 'Aldgate shelter', 
+        price: 30, 
+        description: 'here is a description also', 
+        location: 'further away', 
+        mapurl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2482.775190063914!2d-0.07546948426252068!3d51.51734027963678!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761caf26599a83%3A0x9b451d586c649129!2sMakers!5e0!3m2!1sen!2suk!4v1555754401793!5m2!1sen!2suk"  
+      }
     ]})
   })
 
 	it('shows a list of shelters', () => {
 		cy.visit('/shelters')
-		cy.get('.shelter').should('have.length', 3)
+		cy.get('.shelter').should('have.length', 2)
   })
 
-  it('redirects to shelters route from index', () => {
+  it('shows shelters list in index of site', () => {
     cy.visit('/')
-    cy.get('.shelter').should('have.length', 3)
+    cy.get('.shelter').should('have.length', 2)
 	})
 
 	it('displays info of each shelter', () => {
@@ -39,16 +50,6 @@ describe('find Shelters', () => {
       .contains('here is a description also')
       .get('.shelter-location')
       .contains('further away')
-
-    cy.get('.shelter').eq(2)
-      .get('.shelter-name')
-      .contains('Some shelter')
-      .get('.shelter-price')
-      .contains('20')
-      .get('.shelter-description')
-      .contains('here is a description again')
-      .get('.shelter-location')
-      .contains('farthest away')
 	})
 
   it('returns map of each shelter', () => {
@@ -58,9 +59,6 @@ describe('find Shelters', () => {
       .get('.shelter-map')
       .get('iframe')
     cy.get('.shelter').eq(1)
-      .get('.shelter-map')
-      .get('iframe')
-    cy.get('.shelter').eq(2)
       .get('.shelter-map')
       .get('iframe')
   })
